89e20a0c677dbba2a4f29124a78f9aeed6ac7445
Noel Xavier Ulpindo <noel.ulpindo@gmail.com>
2022-07-15 01:19:42 +0800

    change background

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'landing-page': "url('../assets/img/background.jpg')",
+        'landing-page': "url('https://images.unsplash.com/photo-1464746133101-a2c3f88e0dd9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1143&q=80')",
       }
     },
   },
